549c3ffa55e118f4f7b562fcaa5507dd1c3b0058
Jake Kaad <kaadalac@gmail.com>
2015-05-02 21:01:09 -0700

    add business and image models with db seeds

--- app/models/model.rb
+++
@@ -1,3 +0,0 @@
-class image < ActiveRecord::Base
-
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tilt = Business.create(subscribed: false, name: "Tiltitup", )
+
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt, url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/11176204_458996540930237_138886560_n.jpg"
+)
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11208213_102106636790143_1030741924_n.jpg"
+)
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xta1/t51.2885-15/e15/11176354_1473235036300908_292703387_n.jpg"
+)
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/11203438_359509740908991_382534841_n.jpg"
+)
+
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11116923_759020694216346_1512061545_n.jpg"
+)
+
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11193098_1590399724570701_1861721234_n.jpg"
+)
+
+
+Image.create(location: "TILT - Handcrafted Food & Drink, Built For The American Workforce.", business: tilt,
+  url: "https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e15/11199453_1627103934190662_15413197_n.jpg"
+)
+
+lolo = Business.create(subscribed: true, name: "House of Lolo", message: "LA Market for SPRING 2015 is a wrap, and we can’t wait for our Buyer’s picks to hit the racks! While we have some cold, damp days ahead to get through, we had to share our favorite trends that you can expect to see at lolo come 2015! - See more at: http://houseoflolo.com/blog/#sthash.YTeJrzC2.dpuf")
+
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11208598_1431176860518580_498537553_n.jpg"
+  )
+
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11184620_1436546889973554_1099680532_n.jpg"
+  )
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e15/11189312_460635647428882_710679700_n.jpg"
+  )
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11176136_974357449249841_1821277976_n.jpg"
+  )
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e15/11203221_1653967194832574_1871023242_n.jpg"
+  )
+
+Image.create(location: "House of Lolo - Portland, Oregon", business: lolo,
+  url: "https://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/e15/11190746_820067038048533_551521211_n.jpg"
+  )
+
+
